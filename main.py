a6c0bf2981d55e0eab2d5929ba3e8a04b5f23983
behrad <behradbadeli@gmail.com>
2025-03-05 09:55:35 +0330

    new style

--- main.py
+++ main.py
@@ -3,30 +3,40 @@ import numpy as np
 
 class skewLines(ThreeDScene):
     def construct(self):
-        v1 = np.array([3,1,2])
-        v2 = np.array([2,1,-2])
-        p1 = np.array([0,-1,1])
-        p2 = np.array([1,2,1])
+        v1 = np.array([2,1,-0.5])
+        v2 = np.array([-1,2,-1])
+        p1 = np.array([0,0,1])
+        p2 = np.array([0,0,5])
 
-        t = 0.9
-        c = 0.6
-        axeslen = 6
-
-        l1 = Line3D(start=(c*(p1 - t*v1)), end=(c*(p1 + t*v1)), color=RED, fill_opacity=0.7)
-        l2 = Line3D(start=(c*(p2 + 0.8*t*v2)), end=(c*(p2 - (1/0.8)*t*v2)), color=BLUE, fill_opacity=0.7)
-
-        self.set_camera_orientation(phi=70 * DEGREES, theta= -50 * DEGREES, zoom=1, focal_distance=5)
-
-        axes = ThreeDAxes(x_range=(-1*axeslen,axeslen,1), y_range=(-1*axeslen,axeslen,1), z_range=(-1*axeslen,axeslen,1), x_length=axeslen, y_length=axeslen, z_length=axeslen)
-        labels = axes.get_axis_labels(
-            Text("x-axis").scale(0.7), Text("y-axis").scale(0.45), Text("z-axis").scale(0.45)
-        )
-        self.play(Create(axes, run_time=2))
+        t = 3
+        c = 0.1
+        axeslen = 3
         
-        self.play(Create(labels, run_time=0.5))
+        l1 = Line3D(start=(c*(p1 - t*v1)), end=(c*(p1 + t*v1)), color=RED, fill_opacity=0.7)
+        l2 = Line3D(start=(c*(p2 + t*v2)), end=(c*(p2 - t*v2)), color=BLUE, fill_opacity=0.7)
+
+        self.set_camera_orientation(phi=0 * DEGREES, theta= -90 * DEGREES, zoom=1.2, focal_distance=80)
+
+        axes = ThreeDAxes(x_range=(-1*axeslen,axeslen,1), y_range=(-1*axeslen,axeslen,1), z_range=(0,axeslen,1), x_length=axeslen, y_length=axeslen, z_length=axeslen//2)
+        labels = axes.get_axis_labels(
+            Text("x").scale(0.7), Text("y").scale(0.45), Text("z").scale(0.45)
+        )
+        number_plane = NumberPlane(x_range=(-1*axeslen+1,axeslen-1,1), y_range=(-1*axeslen+1,axeslen-1,1), x_length=axeslen-1, y_length=axeslen-1, faded_line_ratio=3)
+        self.add(axes)
+        self.add(number_plane)
+        
+        self.add(labels)
+        # self.play(GrowFromCenter(l1))
+        # self.play(GrowFromCenter(l2))
+
+        self.add(l1,l2)
+        self.wait(2)
+
+        self.set_camera_orientation(phi= 60 * DEGREES, theta=-30 * DEGREES, zoom=1.1, focal_distance=80, frame_center=[0,0,2])
+        # self.begin_ambient_camera_rotation(rate=0.02, about='theta')
+        self.wait(2)
+        self.set_camera_orientation(phi= 60 * DEGREES, theta=-160 * DEGREES, zoom=1.1, focal_distance=80, frame_center=[0,0,2])
+        self.wait(2)
 
 
-        self.begin_ambient_camera_rotation(rate=0.1, about='theta')
-        self.play(Create(l1, run_time=1))
-        self.play(Create(l2, run_time=1))
-        self.wait(10)
+        self.wait(3)
