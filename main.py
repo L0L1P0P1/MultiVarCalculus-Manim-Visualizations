cb0b88ab02c84b3f327d28ba75ae0120cd437693
L0L1P0P1 <behradbadeli@gmail.com>
2025-03-07 01:23:25 +0330

    adding text

--- main.py
+++ main.py
@@ -3,33 +3,44 @@ import numpy as np
 
 class skewLines(ThreeDScene):
     def construct(self):
+        self.camera.light_source = Point((0,0,0))
         self.camera.fps = 90
+        self.frame.reorient(phi_degrees=0, theta_degrees= -90, center=(0,0,2)) # Axes and NumberPlane
+
+        # Intro
+        title = Text("Skew Lines And Planes", alignment="center", font="Consolas", font_size=48).fix_in_frame().move_to(UP)
+        title_group = VGroup(title, Text("a calculus II project", font_size=24).fix_in_frame().next_to(title,DOWN))
+
+        self.play(Write(title_group))
+        self.wait(1)
+        self.play(title_group.animate.scale(0.5).to_corner(UP))
+
+        # Axes and Number Plane
+        axes = ThreeDAxes(x_range=(-1*axeslen,axeslen,4), y_range=(-1*axeslen,axeslen,4), z_range=(0,axeslen,2), z_axis_config={"include_ticks":False})
+        number_plane = NumberPlane(x_range=(-1*axeslen,axeslen,4), y_range=(-1*axeslen,axeslen,4), faded_line_ratio=4, background_line_style=dict(stroke_color=BLUE_D, stroke_width=2, stroke_opacity=0.6))
+
+        self.play(GrowFromCenter(number_plane), run_time=2)
+        self.add(axes)
+
+        # points and vectors
         v1 = np.array([2,1,-0.5])
         v2 = np.array([-1,2,-1])
-        p1 = np.array([0,0,12])
-        p2 = np.array([0,0,8])
-
+        p1 = np.array([0,0,4])
+        p2 = np.array([0,0,2])
         t = 20
         c = 1
         axeslen = 256
-        
-        l1 = Line3D(start=(c*(p1 - t*v1)), end=(c*(p1 + t*v1)), color=RED)
-        l2 = Line3D(start=(c*(p2 + t*v2)), end=(c*(p2 - t*v2)), color=GREEN)
+        # Lines
+        l1 = Line3D(start=(c*(p1 - t*v1)), end=(c*(p1 + t*v1)), color=RED, width=0.05)
+        l2 = Line3D(start=(c*(p2 + t*v2)), end=(c*(p2 - t*v2)), color=GREEN, width=0.05)
+        linegroup = Group(l1, l2)
 
-        self.frame.reorient(phi_degrees=0, theta_degrees= -90, height=30, center=(0,0,8)) # Axes and NumberPlane
-        axes = ThreeDAxes(x_range=(-1*axeslen,axeslen,4), y_range=(-1*axeslen,axeslen,4), z_range=(0,axeslen,2), z_axis_config={"include_ticks":False})
-        number_plane = NumberPlane(x_range=(-1*axeslen,axeslen,8), y_range=(-1*axeslen,axeslen,8), faded_line_ratio=4, background_line_style=dict(stroke_color=BLUE_D, stroke_width=1.5, stroke_opacity=0.6))
-
-        self.play(GrowFromCenter(axes), run_time=2)
-        self.play(GrowFromCenter(number_plane), run_time=2)
-        self.play(GrowFromCenter(l1))
-        self.play(GrowFromCenter(l2))
-        
+        self.play(GrowFromCenter(linegroup), lag_ratio=1)
         self.wait(2)
+
 
         self.play(self.frame.animate.reorient(phi_degrees=60, theta_degrees= -30), run_time=2)
         self.wait(2)
-
         self.play(self.frame.animate.reorient(phi_degrees=60, theta_degrees= -150), run_time=2)
         self.wait(2)
 
