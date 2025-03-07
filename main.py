4072bf2bce150cc2626b14d9acf52628c9968e5f
L0L1P0P1 <behradbadeli@gmail.com>
2025-03-07 03:51:49 +0330

    did equations and stuff

--- main.py
+++ main.py
@@ -4,20 +4,36 @@ import numpy as np
 class skewLines(ThreeDScene):
     def construct(self):
         self.camera.light_source = Point((0,0,0))
-        self.camera.fps = 90
+        self.camera.fps = 60
+        self.frame.set_field_of_view(70)
         self.frame.reorient(phi_degrees=0, theta_degrees= -90, center=(0,0,2)) # Axes and NumberPlane
 
         # Intro
-        title = Text("Skew Lines And Planes", alignment="center", font="Consolas", font_size=48).fix_in_frame().move_to(UP)
+        title = Text("Skew Lines And Planes", font="Consolas", font_size=48).fix_in_frame().move_to(UP)
         title_group = VGroup(title, Text("a calculus II project", font_size=24).fix_in_frame().next_to(title,DOWN))
 
+        self.bring_to_front(title_group)
         self.play(Write(title_group))
         self.wait(1)
-        self.play(title_group.animate.scale(0.5).to_corner(UP))
+        self.play(FadeOut(title_group), run_time=0.5)
+
+        objective1 = TexText(R"Find two skew lines in $\mathbb{R}^{3}$",).fix_in_frame().move_to(UP)
+        self.play(Write(objective1))
+        self.wait(0.5)
+        self.play(objective1.animate.scale(0.5).to_corner(UR))
+        self.bring_to_front(objective1)
+        eq_text = TexText(R"Vector equations for lines $\mathit{l}_{1}$ and $\mathit{l}_{2}$:", font_size=32).fix_in_frame().shift(UP)
+        self.play(FadeIn(eq_text), run_time=0.5)
+        line_eq = Tex(R"\begin{cases} \mathit{l}_{1}: r_1 + t \  \vec{v}_1 \\ \mathit{l}_2: r_2+s \ \vec{v}_2 \end{cases}", font_size=48, isolate=[R"\vec{v}_2", R"\vec{v}_1"]).fix_in_frame()
+        self.play(Write(line_eq), run_time=2)
+        self.play(FadeOut(eq_text), run_time=0.5)
+        self.play(line_eq.animate.scale(0.5).to_corner(UL))
 
         # Axes and Number Plane
+        axeslen = 256
         axes = ThreeDAxes(x_range=(-1*axeslen,axeslen,4), y_range=(-1*axeslen,axeslen,4), z_range=(0,axeslen,2), z_axis_config={"include_ticks":False})
-        number_plane = NumberPlane(x_range=(-1*axeslen,axeslen,4), y_range=(-1*axeslen,axeslen,4), faded_line_ratio=4, background_line_style=dict(stroke_color=BLUE_D, stroke_width=2, stroke_opacity=0.6))
+        number_plane = NumberPlane(x_range=(-1*axeslen,axeslen,4), y_range=(-1*axeslen,axeslen,4), faded_line_ratio=4, background_line_style=dict(stroke_color=BLUE_D, stroke_width=2, stroke_opacity=1))
+        self.bring_to_back(number_plane)
 
         self.play(GrowFromCenter(number_plane), run_time=2)
         self.add(axes)
@@ -29,7 +45,6 @@ class skewLines(ThreeDScene):
         p2 = np.array([0,0,2])
         t = 20
         c = 1
-        axeslen = 256
         # Lines
         l1 = Line3D(start=(c*(p1 - t*v1)), end=(c*(p1 + t*v1)), color=RED, width=0.05)
         l2 = Line3D(start=(c*(p2 + t*v2)), end=(c*(p2 - t*v2)), color=GREEN, width=0.05)
@@ -38,10 +53,29 @@ class skewLines(ThreeDScene):
         self.play(GrowFromCenter(linegroup), lag_ratio=1)
         self.wait(2)
 
-
-        self.play(self.frame.animate.reorient(phi_degrees=60, theta_degrees= -30), run_time=2)
+        self.play(self.frame.animate.reorient(phi_degrees=75, theta_degrees= 80), run_time=2)
         self.wait(2)
-        self.play(self.frame.animate.reorient(phi_degrees=60, theta_degrees= -150), run_time=2)
+        self.play(self.frame.animate.reorient(phi_degrees=85, theta_degrees= -85), run_time=2)
         self.wait(2)
-
+        self.play(FadeOut(objective1), run_time=1)
         self.wait(3)
+        self.play(FadeOut(axes), FadeOut(number_plane), FadeOut(linegroup))
+        
+        cross_prod_text = TexText(R"""
+                                  Finding the perpendicular vector to $\vec{v}_1$ and $\vec{v}_1$ \\
+                                  as the normal vector for two parallel planes \\ 
+                                  which each include the lines $\mathit{l}_{1}$ and $\mathit{l}_{2}$
+                                  """, font_size=24).fix_in_frame()
+        self.play(Write(cross_prod_text))
+        self.play(cross_prod_text.animate.to_corner(UP))
+        cross_prod = VGroup(
+                Tex(R"\vec{v}_1 \times \vec{v}_2").fix_in_frame(), 
+                Tex(R"\vec{v}_1 \times \vec{v}_2 = \begin{vmatrix} i & j & k \\ x_1 & y_1 & z_1 \\ x_2 & y_2 & z_2 \end{vmatrix} = \vec{n}").fix_in_frame(),
+                Tex(R"\vec{v}_1 \times \vec{v}_2 = \vec{n}").fix_in_frame())
+
+        self.play(TransformMatchingTex(line_eq.copy(), cross_prod[0]))
+        self.play(TransformMatchingTex(cross_prod[0], cross_prod[1]))
+        self.play(TransformMatchingTex(cross_prod[1], cross_prod[2]))
+        self.play(cross_prod[2].animate.scale(0.5).next_to(line_eq,DOWN))
+
+        self.play(FadeIn(axes), FadeIn(number_plane), FadeIn(linegroup))
