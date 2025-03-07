60caf085a8c1a82d930e41fb3c054e728209f789
L0L1P0P1 <behradbadeli@gmail.com>
2025-03-08 00:22:32 +0330

    alot

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import numpy as np
 class skewLines(ThreeDScene):
     def plane_eq(self, u, v, n, p):
         c = n[2] 
-        # Avoid division by zero
         if abs(c) < 1e-10:
             c = 1e-10
         x = u
@@ -13,11 +12,30 @@ class skewLines(ThreeDScene):
         
         return np.array([x, y, z])
 
+    def get_vector(self, start, end, color=YELLOW):
+        line = Line3D(start=start, end=end, color=color)
+        
+        # Direction vector for the arrow
+        direction = end - start
+        length = np.linalg.norm(direction)
+        normalized_direction = direction / length
+        
+        # Create arrow tip (cone pointing in the right direction)
+        cone_height = min(0.2, length * 0.3)  # Adjust tip size
+        cone = Cone(axis=normalized_direction, height=cone_height, radius=cone_height/2)
+        cone.set_color(color)
+        
+        # Position the cone at the end of the line
+        cone.shift(end - (normalized_direction * cone_height * 2))
+        
+        return Group(line, cone)
+
     def construct(self):
-        self.camera.light_source = Point((100,100,100))
+        self.camera.light_source = Point((5,5,0))
         self.camera.fps = 60
         self.frame.set_field_of_view(70)
         self.frame.reorient(phi_degrees=0, theta_degrees= -90, center=(0,0,2)) # Axes and NumberPlane
+        self.add_sound("Vincent Rubinetti - Zeta.mp3")
 
         # Intro
         title = Text("Skew Lines And Planes", font="Consolas", font_size=48).fix_in_frame().move_to(UP)
@@ -26,30 +44,33 @@ class skewLines(ThreeDScene):
         self.bring_to_front(title_group)
         self.play(Write(title_group))
         self.wait(1)
-        self.play(FadeOut(title_group), run_time=0.5)
+        self.play(FadeOut(title_group), run_time=0.25)
 
         objective1 = TexText(R"Find two skew lines in $\mathbb{R}^{3}$",).fix_in_frame().move_to(UP)
         self.play(Write(objective1))
-        self.wait(0.5)
+        self.wait(0.25)
         self.play(objective1.animate.scale(0.5).to_corner(UR))
         self.bring_to_front(objective1)
         eq_text = TexText(R"""
-                          Vector equations for lines $\mathit{l}_{1}$ and $\mathit{l}_{2}$ \\
+                          Vector equations for skew lines $\mathit{l}_{1}$ and $\mathit{l}_{2}$ \\
                           where $\vec{v}_1 \nparallel \vec{v}_2$: \\ \ 
                           """, font_size=32).fix_in_frame().move_to(UP)
         self.play(FadeIn(eq_text), run_time=0.5)
-        line_eq = Tex(R"\begin{cases} \mathit{l}_{1}: r_1 + t \  \vec{v}_1 \\ \mathit{l}_2: r_2+s \ \vec{v}_2 \end{cases}", font_size=48, isolate=[R"\vec{v}_2", R"\vec{v}_1"]).fix_in_frame().next_to(eq_text, DOWN)
+        line_eq = Tex(R"\begin{cases} l_1: r_1 + t \  \vec{v}_1 \\ l_2: r_2+s \ \vec{v}_2 \end{cases}", 
+                      font_size=48, 
+                      isolate=[R"\vec{v}_2", R"\vec{v}_1"], 
+                      t2c={"l_1":RED, "l_2": GREEN}).fix_in_frame().next_to(eq_text, DOWN)
         self.play(Write(line_eq), run_time=2)
         self.play(FadeOut(eq_text), run_time=0.5)
         self.play(line_eq.animate.scale(0.5).to_corner(UL))
+
 
         # Axes and Number Plane
         axeslen = 256
         axes = ThreeDAxes(x_range=(-1*axeslen,axeslen,4), y_range=(-1*axeslen,axeslen,4), z_range=(0,axeslen,2), z_axis_config={"include_ticks":False})
         number_plane = NumberPlane(x_range=(-1*axeslen,axeslen,4), y_range=(-1*axeslen,axeslen,4), faded_line_ratio=4, background_line_style=dict(stroke_color=BLUE_D, stroke_width=2, stroke_opacity=1))
         self.bring_to_back(number_plane)
-
-        self.play(GrowFromCenter(number_plane), run_time=2)
+        self.play(GrowFromCenter(number_plane), run_time=1)
         self.add(axes)
 
         # points and vectors
@@ -59,22 +80,26 @@ class skewLines(ThreeDScene):
         p2 = np.array([0,0,2])
         t = 50
         c = 1
-        # Lines
+
+        # Lines in 3D
         l1 = Line3D(start=(c*(p1 - t*v1)), end=(c*(p1 + t*v1)), color=RED, width=0.05)
         l2 = Line3D(start=(c*(p2 + t*v2)), end=(c*(p2 - t*v2)), color=GREEN, width=0.05)
         linegroup = Group(l1, l2)
-
         self.play(GrowFromCenter(linegroup), lag_ratio=1)
         self.wait(2)
-
         self.play(self.frame.animate.reorient(phi_degrees=75, theta_degrees= 80), run_time=2)
         self.wait(2)
         self.play(self.frame.animate.reorient(phi_degrees=85, theta_degrees= -85), run_time=2)
         self.wait(2)
         self.play(FadeOut(objective1), run_time=1)
-        self.wait(3)
+        self.wait(1)
         self.play(FadeOut(axes), FadeOut(number_plane), FadeOut(linegroup))
         
+        # Normal Vector 
+        objective2 = TexText(R"Find parallel planes containing $\mathit{l}_{1}$ and $\mathit{l}_{2}$").fix_in_frame().move_to(UP)
+        self.play(Write(objective2), run_time=2)
+        self.wait(1.5)
+        self.play(objective2.animate.scale(0.4).to_corner(UR))
         cross_prod_text = TexText(R"""
                                   Finding the perpendicular vector to $\vec{v}_1$ and $\vec{v}_1$ \\
                                   as the normal vector for two parallel planes \\ 
@@ -86,27 +111,56 @@ class skewLines(ThreeDScene):
                 Tex(R"\vec{v}_1 \times \vec{v}_2").fix_in_frame(), 
                 Tex(R"\vec{v}_1 \times \vec{v}_2 = \begin{vmatrix} i & j & k \\ x_1 & y_1 & z_1 \\ x_2 & y_2 & z_2 \end{vmatrix} = \vec{n}").fix_in_frame(),
                 Tex(R"\vec{v}_1 \times \vec{v}_2 = \vec{n}").fix_in_frame())
-
+        waitbetweeneq = 1.5
         self.play(TransformMatchingTex(line_eq.copy(), cross_prod[0]))
+        self.wait(waitbetweeneq)
         self.play(TransformMatchingTex(cross_prod[0], cross_prod[1]))
+        self.wait(waitbetweeneq)
         self.play(TransformMatchingTex(cross_prod[1], cross_prod[2]))
-        self.play(cross_prod[2].animate.scale(0.5).next_to(line_eq,DOWN))
+        self.wait(waitbetweeneq)
+        self.play(cross_prod[2].animate.scale(0.5).next_to(line_eq,DOWN), FadeOut(cross_prod_text))
 
-        self.play(FadeIn(axes), FadeIn(number_plane), FadeIn(linegroup))
+        # Lines Redrawn
+        t = 1
+        l1 = Line3D(start=(c*(p1 - t*v1)), end=(c*(p1 + t*v1)), color=RED, width=0.05)
+        l2 = Line3D(start=(c*(p2 + t*v2)), end=(c*(p2 - t*v2)), color=GREEN, width=0.05)
+        # number_plane redrawn
+        axeslen = 16
+        number_plane = NumberPlane(x_range=(-1*axeslen,axeslen,4), y_range=(-1*axeslen,axeslen,4), faded_line_ratio=4, background_line_style=dict(stroke_color=BLUE_D, stroke_width=2, stroke_opacity=1))
+        linegroupre = Group(l1, l2)
+        self.play(FadeIn(linegroup), FadeIn(number_plane))
+        self.play(TransformMatchingParts(linegroup,linegroupre), self.frame.animate.reorient(phi_degrees=80, theta_degrees=-105, center=[0,0,3.5]))
+        n = np.cross(v1,v2)
+        normalvec3D = Group(self.get_vector(p1, p1+(n*0.2)),self.get_vector(p2, p2+(-n*0.2)))
+        self.play(ShowCreation(normalvec3D), run_time=2)
 
-        normal_vec = np.cross(v1,v2)
-
+        surface_size = 2
         plane1 = ParametricSurface(
-                    lambda u, v: self.plane_eq(u,v,normal_vec, p1),
-                    u_range=(-5,5),
-                    v_range=(-5,5),
+                    lambda u, v: self.plane_eq(u,v,n,p1),
+                    u_range=(-surface_size,surface_size),
+                    v_range=(-surface_size,surface_size),
+                    color=RED,
                 )
-        self.add(plane1)
+        plane2 = ParametricSurface(
+                    lambda u, v: self.plane_eq(u,v,n,p2),
+                    u_range=(-surface_size,surface_size),
+                    v_range=(-surface_size,surface_size),
+                    color=GREEN
+                )
+        planes = Group(plane1, plane2)
+        self.play(ShowCreation(planes))
         self.wait(2)
+        self.play(self.frame.animate.reorient(theta_degrees=80), run_time=2)
+        self.wait(1)
+        self.play(FadeOut(objective2))
+        self.play(FadeOut(normalvec3D), FadeOut(planes), FadeOut(linegroupre), FadeOut(number_plane))
+        
+
+        objective3 = TexText(R"Find the distance between $l_1$ and $l_2%").fix_in_frame()
+        self.play(Write(objective3), run_time=2)
 
 
         
-
 
 
 
